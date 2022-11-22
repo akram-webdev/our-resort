da85b6ead3637cd6c61e6ff8cb2ca35b6e532840
Mossahid123 <mossahidhossain309@gmail.comm>
2022-11-22 14:32:13 +0600

    project is updating

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import './App.css';
-import Home from './Pages/Home/Home';
-
-function App() {
+import React, { useEffect } from "react";
+import "./styles/App.scss";
+import { Route, Routes, useLocation } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import NavMobile from "./components/navbar/NavMoblile";
+import Home from "./components/pages/Home";
+import Rooms from "./components/pages/rooms/Rooms";
+import Room from "./components/pages/rooms/Room";
+import Dining from "./components/pages/Dining";
+import Footer from "./components/pages/Footer";
+import About from "./components/pages/About";
+import Tour from "./components/pages/Tour";
+import Booking from "./components/booking/Booking";
+import Available from "./components/booking/Available";
+import Checkout from "./components/booking/Checkout";
+import Confirm from "./components/booking/Confirm";
+import Auth from "./components/auth/Auth";
+import Existing from "./components/booking/Existing";
+import NoPage from "./components/pages/NoPage";
+const App = () => {
+  const location = useLocation();
+  /* const history = useHistory(); */
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
-    <div className="App">
-     <Home />
-    </div>
+    <>
+      <Navbar />
+      <NavMobile />
+      <Routes>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms" exact component={Rooms} />
+        <Route
+          path="/rooms/:id"
+          exact
+          render={(props) => <Room {...props} />}
+        />
+        <Route path="/dining" exact component={Dining} />
+        <Route path="/about" exact component={About} />
+        <Route path="/tours" exact component={Tour} />
+        <Route path="/booking" exact component={Booking} />
+        <Route path="/booking/availability" exact component={Available} />
+        <Route path="/booking/checkout" exact component={Checkout} />
+        <Route path="/booking/confirm" exact component={Confirm} />
+        <Route path="/booking/existing" exact component={Existing} />
+        <Route path="/admin" exact component={Auth} />
+        <Route path="*" exact component={NoPage} />
+      </Routes>
+      <Footer />
+    </>
   );
-}
+};
 
 export default App;
