1cbf97dd14546677ae06bd5a9bf815cf0f4eff83
akram-webdev <akram.webdev@gmail.com>
2022-11-22 19:41:47 +0600

    marg error testing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { useEffect } from "react";
-import "./styles/App.scss";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Navbar from "./components/navbar/Navbar";
-import NavMobile from "./components/navbar/NavMoblile";
-import Home from "./components/pages/Home";
-import Rooms from "./components/pages/rooms/Rooms";
-import Room from "./components/pages/rooms/Room";
-import Dining from "./components/pages/Dining";
+// import Auth from "./components/auth/Auth";
+import Home from "../src/Components/pages/Home";
+import Navbar from "./Components/Navbar/Navbar";
+import NavMobile from "./Components/Navbar/NavMoblile";
 import Footer from "./components/pages/Footer";
-import About from "./components/pages/About";
-import Tour from "./components/pages/Tour";
-import Booking from "./components/booking/Booking";
-import Available from "./components/booking/Available";
-import Checkout from "./components/booking/Checkout";
-import Confirm from "./components/booking/Confirm";
-import Auth from "./components/auth/Auth";
-import Existing from "./components/booking/Existing";
-import NoPage from "./components/pages/NoPage";
+import "./styles/App.scss";
 const App = () => {
   const location = useLocation();
   /* const history = useHistory(); */
@@ -28,8 +17,8 @@ const App = () => {
       <Navbar />
       <NavMobile />
       <Routes>
-        <Route path="/" exact component={Home} />
-        <Route path="/rooms" exact component={Rooms} />
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/rooms" exact component={Rooms} />
         <Route
           path="/rooms/:id"
           exact
@@ -44,7 +33,7 @@ const App = () => {
         <Route path="/booking/confirm" exact component={Confirm} />
         <Route path="/booking/existing" exact component={Existing} />
         <Route path="/admin" exact component={Auth} />
-        <Route path="*" exact component={NoPage} />
+        <Route path="*" exact component={NoPage} /> */}
       </Routes>
       <Footer />
     </>
--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {BrowserRouter as Router} from 'react-router-dom'
+// import {BrowserRouter as Router} from 'react-router-dom'
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware, compose } from "redux";
+import { applyMiddleware, compose, createStore } from "redux";
 import thunk from "redux-thunk";
 import App from "./App";
 import "./styles/index.scss";
 
+import { BrowserRouter } from "react-router-dom";
 import reducers from "./reducers";
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
 
 ReactDOM.render(
   // Provider connects the store (global states) to the entire App
+  <BrowserRouter>
   <Provider store={store}>
-    <Router>
         <App />
-    </Router>
   
-  </Provider>,
+  </Provider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
